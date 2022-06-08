d7af3addd6889d2ccf669fd37cde0d4119c95dea
RubisRodri <rubisrodri@gmail.com>
2022-06-07 19:21:54 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+console.log(validator.isValid);
+let textoT="";
+const boton= document.getElementById("btn");
+boton.addEventListener( "click", function (e){
+  e.preventDefault();
+
+ const creditCarNumber= document.getElementById("numberCard").value;
+ document.getElementById("numberCard").value=""
+ const result = document.getElementById("result");
+
+ 
+
+ // validar tarjeta
+ const isValid = validator.isValid(creditCarNumber);
+  
+ if (isValid === true){
+   textoT=  "valida";
+ } else{
+   textoT="Es tarjeta invalida por favor comunicate con tu proveedor o entidad Bancaria";
+ }
+
+
+//mascara
+let nmaskify = validator.maskify(creditCarNumber);
+result.innerText = "tu tarjeta numero " + nmaskify+ " " + textoT;
+})
+
+//funcion para sumar la cantidad carteras  seleccionados
+
+const checkout= document.getElementById("checkout");
+
+let cuenta = 0;
+function sumarCuenta(valor){
+  cuenta = cuenta + valor;
+  checkout.innerText="Su monto a Pagar es s/"+ cuenta;
+}
+
+window.sumarCuenta= sumarCuenta;
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
-};
+   isValid: (creditCarNumber) =>{
+   //1ro se revierte los numeros de la tarjeta
+   const reverNum = creditCarNumber.split("").reverse();
+   //se crea la variable para que se acumule el resultado y la funcion para recorrer el array
+   let sumaTotal = 0;
+   for ( let i in reverNum){
+      let newNumber = parseInt(reverNum[i]);
+   // se multiplica separa los numeros pares 
+     if((parseInt(i) + 1) % 2 ==0){
+   //los numeros pares se le resta 9 para obtener la suma de los numeros 
+        if((newNumber * 2) > 9){
+             newNumber = (newNumber * 2) - 9;
+    //ek resto de los numeros pares se multiplica por 2       
+          } else{
+            newNumber = newNumber * 2;  
+            } 
+    // se van sumando y colocando en la variable            
+       } 
+        sumaTotal= (sumaTotal) + (newNumber);  
+        
+    }  
+    return (sumaTotal % 10) ===0;  
+   },   
+   //mascara 
+      
+   maskify:(creditCarNumber)=>{
+    let nmaskify = "";
+    for (let i = 0; i < creditCarNumber.length; i++){
+       if (i<= creditCarNumber.length -5){
+          nmaskify = nmaskify + "#";
+       } else{
+          nmaskify = nmaskify + creditCarNumber[i];
+       }
+       
+    }
+     return nmaskify;
+ }
+}
+  
 
+        
 export default validator;
